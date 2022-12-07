5ab52dd1b6199449e58925a86d8062575e80481b
Prerana Shrestha <preranas@lode.one>
2022-12-07 12:33:25 -0800

    Second init

---
+++ config/assets/gem-page-51285852247.js
@@ -0,0 +1,13 @@
+
+
+    
+  
+    (function( jQuery ){
+  var $module = jQuery('#m-1668993731584').children('.module');
+  if(jQuery().gfYoutube) {
+    $module.gfYoutube();
+  }
+})( window.GemQuery || jQuery );
+  
+    
+  
