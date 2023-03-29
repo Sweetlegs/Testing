a6f4582d9f136f405ce6016cfea22eb9a86633d2
Prerana Shrestha <preranas@lode.one>
2023-03-29 12:50:07 -0700

    First initial

--- assets/custom.js
+++ assets/custom.js
@@ -1,68 +1,162 @@
-/*
-* Pipeline Theme
-*
-* Use this file to add custom Javascript to Pipeline.  Keeping your custom
-* Javascript in this fill will make it easier to update Pipeline. In order
-* to use this file you will need to open layout/theme.liquid and uncomment
-* the custom.js script import line near the bottom of the file.
-*
-*/
-
-
-(function() {
-
-  // Below are example event listeners.  They listen for theme events that Pipeline
-  // fires in order to make it easier for you to add customizations.
-
-  // Keep your scripts inside this IIFE function call to avoid leaking your
-  // variables into the global scope.
-
-
-  document.addEventListener('theme:variant:change', function(event) {
-    // You might use something like this to write a pre-order feature or a
-    // custom swatch feature.
-    var variant = event.detail.variant;
-    var container = event.target;
-    if (variant) {
-      console.log('Container ———————— ↓');
-      console.log(container);
-      console.log('Variant —————————— ↓');
-      console.log(variant);
-      // ... update some element on the page
+document.querySelectorAll(".newsletter-form__button").forEach(function(obj) {
+  obj.addEventListener("click", function(e) {
+    if(document.querySelector("#newsletterGuaranteeCheck").checked) {
+    }
+    else {
+      e.preventDefault();
     }
   });
+});
 
-  document.addEventListener('theme:cart:change', function(event) {
-    var cart = event.detail.cart;
-    if (cart) {
-      console.log('Cart ———————————— ↓');
-      console.log(cart);
-      // ... update an app or a custom shipping caluclator
+document.querySelectorAll(".btn-password_notify").forEach(function(obj) {
+  obj.addEventListener("click", function(e) {
+    console.log("clicked");
+    if(document.querySelector("#newsletterGuaranteeCheck_Password").checked) {
+    }
+    else {
+      e.preventDefault();
     }
   });
-  // Fired when page loads to update header values
-  document.addEventListener('theme:cart:init', (e) => {
-    console.log('theme:cart:init');
-    console.log(e);
+});
+
+// function detectSpecialChar(e) {
+//     var k = e.keyCode;
+//     return ((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8   || (k >= 48 && k <= 57));
+// }
+
+// document.querySelectorAll("#CartSpecialInstructions").forEach(function(obj) {
+//   obj.addEventListener("keydown", function(e) {
+//     if(!detectSpecialChar(e)) {
+//       e.preventDefault();
+//     }
+//   });
+// });
+
+const variant_selector = document.querySelector("#variant-selector");
+jQuery(document).ready(function($) {  
+        var handle      = window.location.href.replaceAll("https://shop.chick-fil-a.com/products/", ""); 
+                        if(handle) {
+                            var selector = "button[handle*='" + handle + "']";
+       											console.log('mnop', $(selector).addClass('selected'));
+                        }
+
+  $(document).on("click", ".product-sibling-swatches button", function(e) {
+    const swatchesProduct = $('.product-sibling-swatches').html();
+    let handle = $(this).attr('handle');
+    
+      window.location = `https://shop.chick-fil-a.com/products/${handle}`;
+    
+  })
+  $(document).on("click", "[data-option-buttons] button", function(e) {
+   
+    const variant_selector = document.querySelector("#variant-selector");
+    
+    var ind = 0;
+    var depth = document.querySelectorAll("[data-option-buttons]").length;
+    var opt_val = [];
+    document.querySelectorAll("[data-option-buttons]").forEach(function(group) {
+      var k = 1;
+      if(depth == 1) {
+        k = 0;
+      }
+      if(ind < depth - k) {
+        var v = '';
+        if(group.querySelector("button.selected").hasAttribute("data-value")) {
+          v = group.querySelector("button.selected").getAttribute("data-value").trim();
+        }
+        else if(group.querySelector("button.selected").hasAttribute("data-label")) {
+          v = group.querySelector("button.selected").getAttribute("data-label").trim();
+        }
+        else {
+          v = group.querySelector("button.selected").textContent.trim();
+        }
+        opt_val.push(v);
+      }
+      else {
+        var opts = variant_selector.querySelectorAll("option");
+
+        group.querySelectorAll("button").forEach(function(obj) {
+          var f1 = 0;
+          var v = '';
+          if(obj.hasAttribute("data-value")) {
+            v = obj.getAttribute("data-value").trim();
+          }
+          else if(obj.hasAttribute("data-label")) {
+            v = obj.getAttribute("data-label").trim();
+          }
+          else {
+            v = obj.textContent.trim();
+          }
+          opts.forEach(function(obj1) {
+            var last_op = obj1.getAttribute("data-variant").split(" / ");
+            var f2 = 0;
+
+            for(var i = 0; i < last_op.length - k; i ++) {
+              if(opt_val[i] == last_op[i]) {
+                f2 = 1;
+                break;
+              }
+            }
+
+            if(f2 == 1) {
+              if(v == last_op[last_op.length - 1]) {
+                if(obj1.getAttribute("data-active") == 'true') {
+                  f1 = 1;
+                }
+                return;
+              }
+            }
+          });
+
+          if(f1 == 0) {
+            console.log('this hit sold');
+            obj.classList.add("soldout");
+          }
+          else {
+            console.log('this hit out');
+            obj.classList.remove("soldout");
+          }
+        });
+      }
+      ind ++;
+    });
   });
+  
+  if(variant_selector) {
+    document.querySelector("[data-option-buttons] button.selected")?.click();
+  }
+});
 
-  // Debounced scroll listeners.  Up and down only fire on direction changes
-  // These events are useful for creating sticky elements and popups.
-  document.addEventListener('theme:scroll', e => { console.log(e); });
-  document.addEventListener('theme:scroll:up', e => { console.log(e); });
-  document.addEventListener('theme:scroll:down', e => { console.log(e); });
+  const elements = document.querySelectorAll('.product-item__inner.addon .select-wrapper select')
+// Loop through each element and add an event listener
+for (let i = 0; i < elements.length; i++) {
+  const element = elements[i];
+  element.addEventListener('change', function() {
+    // Do something when the element is clicked
+    console.log('Clicked element:', this.value);
+    console.log('thid ',  this.closest('.addon-container').querySelector('.product-item__quick-add-button').setAttribute('data-product-id', this.value));
 
-  // Debounced resize listener to bundle changes that trigger document reflow
-  document.addEventListener('theme:resize', e => { console.log(e); });
-
-  // Locks and unlocks page scroll for modals and drawers
-  // These are commented out because firing them will lock the page scroll
-  // the lock event must set `detail` to the modal or drawer body so the 
-  // scroll locking code knows what element should maintain scoll. 
-  // document.dispatchEvent(new CustomEvent('theme:scroll:lock', {bubbles: true, detail: scrollableInnerElement}));
-  // document.dispatchEvent(new CustomEvent('theme:scroll:unlock', {bubbles: true}));
+  });
+}
 
 
-  // ^^ Keep your scripts inside this IIFE function call to avoid leaking your
-  // variables into the global scope.
-})();
+document.addEventListener("openQuickAdd", function(e) {
+  console.log("openQuickAdd");
+  const variant_selector = document.querySelector("#variant-selector");
+  
+  if(variant_selector) {
+    document.querySelector("[data-option-buttons] button.selected")?.click();
+  }
+});
+
+  $(document).ready(function() {
+    $('body').on('click', '[name="checkout"], [name="goto_pp"], [name="goto_gc"]', function() {
+      if ($('#agree').is(':checked')) {
+        $(this).submit();
+      }
+      else {
+        alert("Please agree to our Terms of Service and acknowledge receipt of our Privacy Policy and California Privacy Notice to make a purchase.");
+        return false;
+      }
+    });
+  });
